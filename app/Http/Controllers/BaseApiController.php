10d1edefcd8aa24bf65a4b4d7be22d723894304f
Lukas Oppermann <lukas@vea.re>
2014-10-23 14:12:21 +0200

    extracting errors

--- app/Http/Controllers/BaseApiController.php
+++ app/Http/Controllers/BaseApiController.php
@@ -1,8 +1,11 @@
 <?php namespace Formandsystemapi\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Response;
 
 class BaseApiController extends Controller {
+
+	protected statusCode;
 
 	/**
 	* construct
@@ -11,26 +14,85 @@ class BaseApiController extends Controller {
 	*/
 	function __construct()
 	{
-		// header('Access-Control-Allow-Origin: '.Auth::user()->service_url);
-		// header('Access-Control-Allow-Origin: http://cms.formandsystem.com');
 	  // Allow from any origin
 		header('content-type: application/json; charset=utf-8');
-		header('Access-Control-Allow-Origin: http://cms.formandsystem.com');
+		// header('Access-Control-Allow-Origin: http://cms.formandsystem.com');
 		header('Access-Control-Allow-Credentials: true');
 		header('Access-Control-Max-Age: 86400');    // cache for 1 day
 		header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
+	}
 
-    // Access-Control headers are received during OPTIONS requests
-    // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    //
-    //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-    //         header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
-    //
-    //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-    //         header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    //
-    //     exit(0);
-    // }
+	/**
+	 * return the status code
+	 *
+	 * @method getStatusCode
+	 */
+	function getStatusCode()
+	{
+		return $this->statusCode;
+	}
+
+	/**
+	 * set the status code
+	 *
+	 * @method setStatusCode
+	 *
+	 * @param int $statusCode
+	 *
+	 * return $this
+	 */
+	function setStatusCode( $statusCode )
+	{
+		$this->statusCode = $statusCode;
+
+		return $this;
+	}
+
+	/**
+	 * return a response
+	 *
+	 * @method respond
+	 *
+	 * @param  array or string  $data
+	 * @param  array  $headers
+	 *
+	 * @return Illuminate\Support\Facades\Response
+	 */
+	function respond($data, $headers = [])
+	{
+		return Response::json($data, $this->getStatusCode(), $headers);
+	}
+
+	/**
+	 * return an error response with provided message
+	 *
+	 * @method responeWithError
+	 *
+	 * @param  array $message
+	 *
+	 * @return $this->respond
+	 */
+	function responeWithError($message)
+	{
+		return $this->respond([
+			'success' => false,
+			'error' => [
+				'message' => $message,
+				'status_code' => $this->getStatusCode()
+			]
+		]);
+	}
+
+	/**
+	 * respond with not found error
+	 *
+	 * @method respondNotFound
+	 *
+	 * @param  string          $message
+	 */
+	function respondNotFound( $message = "Not Found" )
+	{
+		return $this->setStatusCode(404)->respondWithError($message);
 	}
 
 
