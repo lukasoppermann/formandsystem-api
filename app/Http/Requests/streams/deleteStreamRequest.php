ad715854cda2c711fb76ffac7a65a8736fab3e4f
Lukas Oppermann <lukas@vea.re>
2014-10-29 16:09:20 +0100

    remove lowercase streams

--- app/Http/Requests/streams/deleteStreamRequest.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Formandsystemapi\Http\Requests\Streams;
-
-class deleteStreamRequest extends basicStreamRequest {
-
-	protected $scopes = ['content.read','content.delete'];
-
-}
--- app/Http/Requests/streams/updateStreamRequest.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Formandsystemapi\Http\Requests\Streams;
-
-class updateStreamRequest extends basicStreamRequest {
-
-	protected $scopes = ['content.read','content.write'];
-
-}
