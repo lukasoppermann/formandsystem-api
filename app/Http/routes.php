8c41fcba880c3b150fe2484c44e9fbc091e2f244
Lukas Oppermann <lukas@vea.re>
2014-11-09 19:12:38 +0100

    transition to laravel 5 style in routes & updating oAuth server

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,9 +17,9 @@
 /*
  * access_token
  */
-Route::post('oauth/access_token', function()
+$router->post('oauth/access_token', function()
 {
-    return Authorizer::issueAccessToken();
+  return Authorizer::issueAccessToken();
 });
 
 /*
@@ -27,22 +27,22 @@ Route::post('oauth/access_token', function()
 | Define API v1 routes
 |--------------------------------------------------------------------------
 */
-Route::group(array('prefix' => 'v1', 'before' => ['oauth']), function()
+$router->group(array('prefix' => 'v1', 'before' => ['oauth']), function($router)
 {
   /*
    * Pages resource
    */
-	Route::resource('pages', 'PagesApiController', array('except' => array('create', 'edit')));
+	$router->resource('pages', 'PagesApiController', array('except' => array('create', 'edit')));
 
   /*
    * Stream resource
    */
-  Route::resource('streams', 'StreamsApiController', array('except' => array('create', 'edit')));
+  $router->resource('streams', 'StreamsApiController', array('except' => array('create', 'edit')));
 
   /*
    * Wrong paths
    */
-   Route::any('/', 'BaseApiController@invalidPath');
+   $router->any('/', 'BaseApiController@invalidPath');
 });
 
 /*
@@ -50,4 +50,4 @@ Route::group(array('prefix' => 'v1', 'before' => ['oauth']), function()
 | Path with wrong version number
 |--------------------------------------------------------------------------
 */
-Route::any('/{version?}/{wrong?}/{path?}/{given?}', 'BaseApiController@invalidPath');
+$router->any('/{version?}/{wrong?}/{path?}/{given?}', 'BaseApiController@invalidPath');
