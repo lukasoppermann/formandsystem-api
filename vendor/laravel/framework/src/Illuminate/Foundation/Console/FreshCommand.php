7d301470bb6b9bbc3b8fd541c6f03358e1e42615
Lukas Oppermann <lukas@vea.re>
2014-12-18 09:49:51 +0100

    adding vendors

---
+++ vendor/laravel/framework/src/Illuminate/Foundation/Console/FreshCommand.php
@@ -0,0 +1,91 @@
+<?php namespace Illuminate\Foundation\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class FreshCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'fresh';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = "Remove some of Laravel's scaffolding";
+
+	/**
+	 * The filesystem instance.
+	 *
+	 * @var \Illuminate\Filesystem\Filesystem
+	 */
+	protected $files;
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @param  \Illuminate\Filesystem\Filesystem  $files
+	 * @return void
+	 */
+	public function __construct(Filesystem $files)
+	{
+		parent::__construct();
+
+		$this->files = $files;
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		foreach ($this->getFiles() as $file)
+		{
+			$this->files->delete($file);
+
+			$this->line('<info>Removed File:</info> '.$file);
+		}
+
+		foreach ($this->getDirectories() as $directory)
+		{
+			$this->files->deleteDirectory($directory);
+
+			$this->line('<comment>Removed Directory:</comment> '.$directory);
+		}
+
+		$this->info('Scaffolding Removed!');
+	}
+
+	/**
+	 * Get the files that should be deleted.
+	 *
+	 * @return array
+	 */
+	protected function getFiles()
+	{
+		return [
+			base_path('.bowerrc'),
+			base_path('bower.json'),
+			base_path('gulpfile.js'),
+			base_path('package.json'),
+		];
+	}
+
+	/**
+	 * Get the directories that should be deleted.
+	 *
+	 * @return array
+	 */
+	protected function getDirectories()
+	{
+		return [];
+	}
+
+}
---
+++ vendor/phpspec/phpspec/spec/PhpSpec/Config/OptionsConfigSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace spec\PhpSpec\Config;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class OptionsConfigSpec extends ObjectBehavior
+{
+    function it_says_rerun_is_enabled_when_setting_is_true()
+    {
+        $this->beConstructedWith(false, false, true, false);
+
+        $this->isReRunEnabled()->shouldReturn(true);
+    }
+
+    function it_says_rerun_is_not_enabled_when_setting_is_false()
+    {
+        $this->beConstructedWith(false, false, false, false);
+
+        $this->isReRunEnabled()->shouldReturn(false);
+    }
+
+    function it_says_faking_is_enabled_when_setting_is_true()
+    {
+        $this->beConstructedWith(false, false, false, true);
+
+        $this->isFakingEnabled()->shouldReturn(true);
+    }
+
+    function it_says_faking_is_not_enabled_when_setting_is_false()
+    {
+        $this->beConstructedWith(false, false, false, false);
+
+        $this->isFakingEnabled()->shouldReturn(false);
+    }
+}
