0f216f41c4a00be9d8e1ef73b14aa3498c86cd31
Lukas Oppermann <lukas@vea.re>
2016-05-02 17:18:50 +0200

    change to using collectionable

--- database/migrations/2016_03_07_165505_create_collectionables_table.php
+++ database/migrations/2016_03_07_165505_create_collectionables_table.php
@@ -34,37 +34,3 @@ class CreateCollectionablesTable extends Migration
         Schema::drop('collectionables');
     }
 }
-
-//
-// use Illuminate\Database\Schema\Blueprint;
-// use Illuminate\Database\Migrations\Migration;
-//
-// class CreateCollectionPageTable extends Migration
-// {
-//     /**
-//      * Run the migrations.
-//      *
-//      * @return void
-//      */
-//      public function up()
-//      {
-//          Schema::create('collection_page', function (Blueprint $table) {
-//              $table->increments('id');
-//
-//              $table->uuid('collection_id')->index();
-//              $table->foreign('collection_id')->references('id')->on('collections')->onDelete('cascade');
-//
-//              $table->uuid('page_id')->index();
-//              $table->foreign('page_id')->references('id')->on('pages')->onDelete('cascade');
-//          });
-//      }
-//      /**
-//       * Reverse the migrations.
-//       *
-//       * @return void
-//       */
-//      public function down()
-//      {
-//          Schema::drop('collection_page');
-//      }
-// }
