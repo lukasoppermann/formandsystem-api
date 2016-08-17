e564632a06e66fb486d0f54326add83b4cace41a
Lukas Oppermann <lukas@vea.re>
2016-08-17 20:53:02 +0200

    bugfix

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -11,11 +11,10 @@ return [
     'not_related' => 'The resource ":resource" has no relationship to ":related".',
     'invalid_relationship' => 'Invalid relationship with type ":type" and id ":id".',
     // MISSING Client details
-    'missing_client_database' => 'Please setup a database for your account.',
-    'missing_client_ftp_image' => 'Please setup an image ftp access for your account.',
+    'missing_client_database'   => 'Please setup a database for your account.',
+    'missing_client_ftp_image'  => 'Please setup an image ftp access for your account.',
     'missing_client_ftp_backup' => 'Please setup a backup ftp access for your account.',
+    'missing_client_details'    => 'Please setup the details for your api client in the cms.',
     // INTERNAL 500
     'internal' => 'Failed to authorize the request due to an internal error. Please notify support@formandsystem.com.',
-
-
 ];
